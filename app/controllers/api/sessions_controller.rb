eac2f4c5f8e85014cc37360fa55d8bd8e9c14d61
Kelsang <kelsangrtsering@gmail.com>
2023-04-02 17:54:17 -0400

    work on auth for backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def show
+    @user = current_user 
+    if @user 
+      render 'api/users/show'
+    else 
+      render json: { user: nil }
+    end 
+  end
+
+  def create
+    @user = User::find_by_credentials(params[:credential], params[:password])
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else 
+      render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+    end 
+  end
+
+  def destroy
+    logout! 
+    render json: {message: 'success'}
+  end
+end
