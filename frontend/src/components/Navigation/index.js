2c73c80795ccf830114b1b63cbb4c8b0f0e2b4b3
Kelsang <kelsangrtsering@gmail.com>
2023-04-04 12:36:55 -0400

    work on styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import logo from '../../images/amazonlogo.png';
 import logo2 from '../../images/amazonlogo2.png';
 import magnifyingGlass from '../../images/magnifying_glass.png';
 import './Navigation.css';
