8f95c0d9fc68ee9c1d14cf555d41734a052b6212
Kelsang <kelsangrtsering@gmail.com>
2023-05-05 12:26:30 -0400

    optimize logic for product show

--- frontend/src/components/ProductShow/index.js
+++ frontend/src/components/ProductShow/index.js
@@ -74,16 +74,45 @@ const ProductShow = () => {
         setQuantity(parseInt(e.currentTarget.value));
     }
 
+    let reviewsAmount; 
+
+    if(reviewsCount === 1){
+        reviewsAmount = (
+            <h1>{reviewsCount} rating</h1>
+        )
+    } else {
+        reviewsAmount = (
+            <h1>{reviewsCount} ratings</h1>
+        )
+    }
+
+    let reviewForm;
+
     if(sessionUser){
-        productShow = (
+        reviewForm = (
+            <a href={`/createReview/${product.id}`}>
+                <button id="accountCreation">Write a customer review</button>
+            </a>
+        )
+    } else {
+        reviewForm = (
+            <a href={`/login`}>
+                <button id="accountCreation">Write a customer review</button>
+            </a>
+        )
+    }
+
+    return (
         <>
-            <div id="showMainDiv">
+          <div id="fillerDiv"></div> 
+          <div id="showMainDiv">
                 <div id="imageDiv">
                     <img id="showImage" src={product.photoUrl} alt=""/>
                 </div>
                 <div id="descriptionDiv">
                     <h1 id="productTitle">{product.name}</h1>
                     <h1 id="productRatings">{reviewsAverage}</h1>
+                    {reviewsAmount}
                     <div id="lineSeparator"></div>
                     <h1 id="productPrice1">${product.price}</h1>
                     <span id="freeReturns">FREE Returns</span>
@@ -140,92 +169,9 @@ const ProductShow = () => {
             
             </div>
             <div id="createReviewDiv">
-                <a href={`/createReview/${product.id}`}>
-                    <button id="accountCreation">Write a customer review</button>
-                </a>
+                {reviewForm}
             </div>
             <ReviewIndex product={product}/>
-        </>
-        )
-    } else {
-        productShow = (
-            <>
-                <div id="showMainDiv">
-                    <div id="imageDiv">
-                        <img id="showImage" src={product.photoUrl} alt=""/>
-                    </div>
-                    <div id="descriptionDiv">
-                        <h1 id="productTitle">{product.name}</h1>
-                        <h1 id="productRatings">{reviewsAverage}</h1>
-                        <div id="lineSeparator"></div>
-                        <h1 id="productPrice1">${product.price}</h1>
-                        <span id="freeReturns">FREE Returns</span>
-                        <ul id="descriptionList">
-                            {descriptionArray.map((sentence)=>{
-                                return (
-                                    <li id="sentence" key={Math.random()}>
-                                        {sentence}
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
-                    <div id="transactionDiv">
-                        <h1 id="productPrice2">${product.price}</h1>
-                        <span id="freeReturns">FREE Returns</span>
-                        <span id="freeDelivery">FREE delivery</span>
-                        <span id="inStock">In Stock</span>
-
-                        <form onSubmit={handleAddToCart} id="addToCartForm">
-                            <div id="quantityDiv">
-                                <span id="quantity1">Qty:</span>
-                                <select name="product_quantity" id="quantitySelector" form="addToCartForm" onChange={updateSelector}>
-                                    <option value="1">1</option> 
-                                    <option value="2">2</option> 
-                                    <option value="3">3</option> 
-                                    <option value="4">4</option> 
-                                    <option value="5">5</option> 
-                                </select>
-                            </div>
-                            <button type="submit" id="addToCart">Add to Cart</button>
-                        </form>
-                        <button id="buyNow">Buy Now</button>
-                        <div id="paymentDiv">
-                            <span id="payment">Payment</span>
-                            {/* <a href="/" id="secureTransaction">Secure transaction</a> */}
-                            <span id="secureTransaction">Secure transaction</span>
-                        </div>
-                        <div id="shipsFromDiv">
-                            <span id="shipsFrom">Ships from</span>
-                            <span id="rainierSiteLink">Rainier.com</span>
-                        </div>
-                        <div id="soldByDiv">
-                            <span id="soldBy">Sold by</span>
-                            <span id="rainierSiteLink">Rainier.com</span>
-                        </div>
-                        <div id="returnsDiv">
-                            <span id="returns">Returns</span>
-                            {/* <a href="/" id="eligibleForReturn">Eligible for Return, Refund or Replacement within 30 days of receipt</a> */}
-                            <span id="eligibleForReturn">Eligible for Return, Refund or Replacement within 30 days of receipt</span>
-                        </div>
-                        
-                </div>
-                
-                </div>
-            <div id="createReviewDiv">
-                <a href={`/login`}>
-                    <button id="accountCreation">Write a customer review</button>
-                </a>
-            </div>
-            <ReviewIndex product={product}/>
-        </>
-        )
-    }
-
-    return (
-        <>
-          <div id="fillerDiv"></div> 
-          {productShow}
         </>
     )
 
