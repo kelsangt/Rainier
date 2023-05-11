3fb999a2c3517e26c6e0be279d0ca2b90af8da4a
Kelsang <kelsangrtsering@gmail.com>
2023-05-11 01:23:33 -0400

    format reviews and improve styling

--- frontend/src/components/ReviewIndex/ReviewIndexItem.js
+++ frontend/src/components/ReviewIndex/ReviewIndexItem.js
@@ -1,15 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
-import NotFound from "../NotFound";
 import './ReviewIndexItem.css';
 import userIcon from '../../images/userIcon.png'
 import { deleteReview } from "../../store/reviews";
-import { useEffect } from "react";
-import { fetchProduct, getProduct } from "../../store/products";
+// import { getProduct } from "../../store/products";
 
 const ReviewIndexItem = ({review}) => {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-    const product = useSelector(getProduct(review.productId));
+    // const product = useSelector(getProduct(review.productId));
   
 
     const handleReviewDelete = (e) => {
@@ -19,27 +17,42 @@ const ReviewIndexItem = ({review}) => {
     
     let reviewItem;
 
+    let stars = [];
+
+    for(let i = 0; i < review.rating; i++){
+        stars.push(<i id="starIcon" className="fa fa-star"></i>);
+    }
+
+    let reviewRemainder = 5 - review.rating;
+
+    for(let j = 0; j < reviewRemainder; j++){
+        stars.push(<i id="starIcon" className="fa fa-star-o"></i>);
+    }
+
+
     if(sessionUser && review.userId === sessionUser.id){
         reviewItem = (
             <>
             <div id="reviewShow">
-                <div>
+                <div id="userInfoDiv">
                     <img src={userIcon} alt="usericon" className="userIcon" /> 
-                    {review.name}
+                    <h1 id="reviewNameH1">{review.name}</h1>
                 </div>
                 <div id="ratingTitle">
-                    {review.rating}
-                    {review.title}
+                    <h1>{stars}</h1>
+                    <h1 id="reviewTitleH1">{review.title}</h1>
                 </div>
-                <div>{review.body}</div>
+                <div id="verifiedPurchaseDiv">
+                    <h1>Verified Purchase</h1>
+                </div>
+                <div id="reviewBodyDiv">{review.body}</div>
+                <div id="reviewButtons">
+                    <a href={`/editReview/${review.id}`}>
+                        <button id="editReviewButton">Edit</button>
+                    </a>
+                    <button id="deleteReviewButton" onClick={handleReviewDelete}>Delete</button>
+                </div>
             </div>
-            <button onClick={handleReviewDelete}>Delete</button>
-            <div>
-                <a href={`/editReview/${review.id}`}>
-                    <button>Edit</button>
-                </a>
-            </div>
-           
             </>
         )
         
@@ -47,15 +60,18 @@ const ReviewIndexItem = ({review}) => {
         reviewItem = (
             <>
             <div id="reviewShow">
-                <div>
+                <div id="userInfoDiv">
                     <img src={userIcon} alt="usericon" className="userIcon" /> 
-                    {review.name}
+                    <h1 id="reviewNameH1">{review.name}</h1>
                 </div>
                 <div id="ratingTitle">
-                    {review.rating}
-                    {review.title}
+                    <h1>{stars}</h1>
+                    <h1 id="reviewTitleH1">{review.title}</h1>
                 </div>
-                <div>{review.body}</div>
+                <div id="verifiedPurchaseDiv">
+                    <h1>Verified Purchase</h1>
+                </div>
+                <div id="reviewBodyDiv">{review.body}</div>
             </div>
             </>
         )
